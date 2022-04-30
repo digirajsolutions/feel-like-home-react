3cc2662fd2aae133577dab8536db1003ebb5d7a0
Digiraj Solutions <66156041+digirajsolutions@users.noreply.github.com>
2022-04-30 12:51:29 +0530

    User sign in functionality.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getAuth } from 'firebase/auth'
 
 function Profile() {
-  return <div>Profile</div>
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+
+  return user ? <h2>Welcome {user.displayName}</h2> : 'Not Logged In'
 }
 
 export default Profile
